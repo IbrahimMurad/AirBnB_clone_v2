9c6aa4bf3a531ee9ee4e9e95985247cf6f1788cd
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-02-20 07:46:40 +0200

    3. MySQL setup development

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,11 @@
+-- create a new database hbnb_dev_db for AirBnB clone project
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- create a new user hbnb_dev in the localhost for AirBnB clone project
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- grant hbnb_dev all privileges on the database hbnb_dev_db
+GRANT * ON 'hbnb_dev_db'.* TO 'hbnb_dev'@'localhost';
+
+-- grant hbnb_dev SELECT privileges on the database performance_schema
+GRANT SELECT ON 'performance_schema'.* TO 'hbnb_dev'@'localhost';
