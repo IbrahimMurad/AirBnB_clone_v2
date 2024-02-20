486a8b0cb98bfde1cc0af4e5e12aeb038f51e7b5
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-02-20 07:48:12 +0200

    3. MySQL setup development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,7 +5,7 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- grant hbnb_dev all privileges on the database hbnb_dev_db
-GRANT * ON 'hbnb_dev_db'.* TO 'hbnb_dev'@'localhost';
+GRANT * ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 -- grant hbnb_dev SELECT privileges on the database performance_schema
-GRANT SELECT ON 'performance_schema'.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
