0e2eae4c9b48d486d5b04c3ec7dd47b25fbc986a
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-02-21 18:44:26 +0200

    8. DBStorage - Place

--- models/place.py
+++ models/place.py
@@ -5,7 +5,7 @@ from models import storage
 from models.base_model import BaseModel, Base
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
     city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
