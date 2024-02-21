094bfddc9e72373418d774e3709220c5a1f56d4a
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-02-21 20:10:48 +0200

    9. DBStorage - Review

--- console.py
+++ console.py
@@ -130,7 +130,7 @@ class HBNBCommand(cmd.Cmd):
             for para in parameters:
                 key, _, value = para.partition('=')
                 if value[0] == '"':
-                    value = value[1:-1].replace('=', ' ').replace('"', '\"')
+                    value = value[1:-1].replace('=', ' ').replace('"', '\\"')
                     kwargs[key] = value
                 else:
                     try:
@@ -207,7 +207,7 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         try:
-            storage.all()[key].delete()
+            del (storage.all()[key])
             storage.save()
         except KeyError:
             print("** no instance found **")
