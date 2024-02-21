dbdce7a87f738d601f8a3b6fbfd64a69aa713944
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-02-21 22:01:00 +0200

    9. DBStorage - Review

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, orm
+from sqlalchemy import Column, String, orm
+from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
@@ -9,4 +10,4 @@ class Amenity(BaseModel, Base):
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
 
-    place_amenities = orm.relationship("Place")
+    place_amenities = orm.relationship("Place", secondary=place_amenity)
--- models/place.py
+++ models/place.py
@@ -5,32 +5,34 @@ from models import storage
 from models.base_model import BaseModel, Base
 
 
-class Place(BaseModel, Base):
-    """ A place to stay """
-    __tablename__ = "places"
-    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, default=0)
-    longitude = Column(Float, default=0)
-    amenity_ids = []
-
-    reviews = orm.relationship("Review", cascade="all, delete, delete-orphan",
-                               backref="place")
-
-    place_amenity = Table('place_amenity', Base.metadata,
+place_amenity = Table('place_amenity', Base.metadata,
     Column('place_id', String(60), ForeignKey('places.id'),
            primary_key=True, nullable=False),
     Column('amenity_id', String(60), ForeignKey('amenities.id'),
            primary_key=True, nullable=False)
 )
 
-    amenities = orm.relationship("Amenity", secondary="place_amenity", viewonly=False)
+
+class Place(BaseModel, Base):
+    """ A place to stay """
+    __tablename__ = "places"
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024))
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float)
+    longitude = Column(Float)
+    amenity_ids = []
+
+    reviews = orm.relationship("Review", cascade="all, delete, delete-orphan",
+                               backref="place")
+
+    amenities = orm.relationship("Amenity", secondary=place_amenity,
+                                 viewonly=False, back_populates="place_amenities")
 
     @property
     def reviews(self):
@@ -47,12 +49,7 @@ with place_id equals to the current Place.id"""
     def amenities(self):
         """getter attribute that returns the list of Amenity instances
 based on the attribute amenity_ids that contains all Amenity.id linked to the Place"""
-        from models.amenity import Amenity
-        place_amenities = []
-        for amenity in storage.all(Amenity).values():
-            if amenity.place_id == self.id:
-                place_amenities.append(amenity)
-        return place_amenities
+        return self.amenity_ids
 
     @amenities.setter
     def amenities(self, value=None):
