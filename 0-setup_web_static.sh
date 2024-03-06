75a0a73383715f695b8882a48791c3072c29c18d
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-03-06 21:26:47 +0200

    setup web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,15 +7,15 @@ mkdir -p /data/web_static/releases
 mkdir -p /data/web_static/shared
 mkdir -p /data/web_static/releases/test
 echo "<html>
-        <head>
-        </head>
-        <body>
-                Holberton School
-        </body>
+	<head>
+	</head>
+	<body>
+		Holberton School
+	</body>
 </html>" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 my_web_static="\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/;\n\t\tautoindex off;\n\t}
 "
-sed -i "/       server_name _;/ a/$my_web_static" /etc/nginx/sites-enabled/default
+sed -i "/	server_name _;/ a\ $my_web_static" /etc/nginx/sites-enabled/default
 service nginx restart
