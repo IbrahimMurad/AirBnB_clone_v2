1adeed7de620aff72d56d679c485463bf41dc8d9
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-03-06 20:29:54 +0200

    pack web static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This is a fabfile used for packing web_static folder"""
+
+def do_pack():
+    """ Packes web_static into .tgz archive """
+
+    from datetime import datetime
+    current_moment = datetime.now()
+    arch_name = "web_static_{}{}{}{}{}{}.tgz".format(
+        current_moment.year,
+        current_moment.month,
+        current_moment.day,
+        current_moment.hour,
+        current_moment.minute,
+        current_moment.second
+    )
+    local("tar -cvzf versions/{} web_static".format(arch_name))
