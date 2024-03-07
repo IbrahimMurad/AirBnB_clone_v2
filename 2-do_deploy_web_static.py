7e1106fabc866ebcb67c0c2d621815d1d7d90866
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-03-07 14:45:41 +0200

    2. Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -14,40 +14,31 @@ def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
 
-    # path strings
-    releases = "/data/web_static/releases"
-    arch_name = archive_path.split('/')[-1].split('.')[0]
-    full_path = "{}/{}".format(releases, arch_name)
+    try:
+        # path strings
+        releases = "/data/web_static/releases"
+        arch_name = archive_path.split('/')[-1].split('.')[0]
+        full_path = "{}/{}".format(releases, arch_name)
 
-    # transfering the archive to the servers
-    if put(local_path=archive_path, remote_path='/tmp/').failed:
+        # transfering the archive to the servers
+        put(local_path=archive_path, remote_path='/tmp/')
+
+        # unpacking the archive in a new folder with the same name
+        run("mkdir -p {}/".format(full_path))
+        run("tar -xzf /tmp/{}.tgz -C {}/".format(arch_name, full_path))
+
+        # deleting the the archive
+        run("rm /tmp/{}.tgz".format(arch_name))
+
+        # moving the content of web_static in the new folder
+        run("mv {0}/web_static/* {0}/".format(full_path))
+        run("rm -rf {}/web_static".format(full_path))
+
+        # redirecting the symlink of current to the new folder
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}/ /data/web_static/current".format(full_path))
+
+        print("New version deployed!")
+        return True
+    except Exception:
         return False
-
-    # unpacking the archive in a new folder with the same name
-
-    if run("mkdir -p {}/".format(full_path)).failed:
-        return False
-
-    if run("tar -xzf /tmp/{}.tgz -C {}/".format(arch_name, full_path)).failed:
-        return False
-
-    # deleting the the archive
-    if run("rm /tmp/{}.tgz".format(arch_name)).failed:
-        return False
-
-    # moving the content of web_static in the new folder
-    if run("mv {0}/web_static/* {0}/".format(full_path)).failed:
-        return False
-
-    if run("rm -rf {}/web_static".format(full_path)).failed:
-        return False
-
-    # redirecting the symlink of current to the new folder
-    if run("rm -rf /data/web_static/current").failed:
-        return False
-
-    if run("ln -s {}/ /data/web_static/current".format(full_path)).failed:
-        return False
-
-    print("New version deployed!")
-    return True
