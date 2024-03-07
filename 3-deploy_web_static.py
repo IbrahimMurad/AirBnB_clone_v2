ade17e7bbaf9dc04ef1c5e9396955f2af915b682
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-03-07 18:10:40 +0200

    Full deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,15 +3,14 @@
 creates and distributes an archive to your web servers,
 using the function deploy
 """
-
+do_pack = __import__("1-pack_web_static").do_pack
+do_deploy = __import__("2-do_deploy_web_static").do_deploy
 
 def deploy():
     """ Packes web_static into .tgz archive """
 
-    do_pack = __import__('1-pack_web_static').do_pack
-    do_deploy = __import__('2-do_deploy_web_static').do_deploy
     arch_path = do_pack()
-    if arch_path is False:
+    if arch_path is None:
         return False
     else:
         return do_deploy(arch_path)
