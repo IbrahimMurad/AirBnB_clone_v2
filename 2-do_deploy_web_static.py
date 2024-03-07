8710517539d72a501a2ddc6eeab5f2d58203b66d
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-03-07 15:05:43 +0200

    2. Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,28 +17,17 @@ def do_deploy(archive_path):
     try:
         # path strings
         releases = "/data/web_static/releases"
-        arch_name = archive_path.split('/')[-1]
+        arch_name = os.path.basename(archive_path)
         no_extension = arch_name.split('.')[0]
         full_path = "{}/{}".format(releases, no_extension)
-
-        # transfering the archive to the servers
         put(archive_path, '/tmp/')
-
-        # unpacking the archive in a new folder with the same name
         run("mkdir -p {}/".format(full_path))
         run("tar -xzf /tmp/{} -C {}/".format(arch_name, full_path))
-
-        # deleting the the archive
         run("rm /tmp/{}".format(arch_name))
-
-        # moving the content of web_static in the new folder
         run("mv {0}/web_static/* {0}/".format(full_path))
         run("rm -rf {}/web_static".format(full_path))
-
-        # redirecting the symlink of current to the new folder
         run("rm -rf /data/web_static/current")
         run("ln -s {}/ /data/web_static/current".format(full_path))
-
         print("New version deployed!")
         return True
     except Exception:
