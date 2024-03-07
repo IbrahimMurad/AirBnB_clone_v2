198bbf2fe3065c54163f886947d752438c2c5fa7
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-03-07 09:59:49 +0200

    2. Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This is a fabfile used for distributes an archive to my web servers"""
-from fabric.api import *
+from fabric.api import put, run, env
 
 
 env.hosts = ['100.25.170.65', '54.172.165.136']
@@ -19,37 +19,29 @@ def do_deploy(archive_path):
     try:
         # path strings
         releases = "/data/web_static/releases"
-        arch_name = archive_path.rsplit('/', 1)[1].split('.')[0]
+        arch_name = archive_path.rsplit('/', 1)[1].partition('.tgz')[0]
 
         # transfering the archive to the servers
-        if put(local_path=archive_path, remote_path='/tmp/').failed:
-            raise Exception
+        put(local_path=archive_path, remote_path='/tmp/')
 
         # unpacking the archive in a new folder with the same name
-        if run("mkdir -p {0}/{1}/".format(releases, arch_name)).failed:
-            raise Exception
-        if run("tar -xzf /tmp/{1}.tgz -C {0}/{1}/".format(
+        run("mkdir -p {0}/{1}/".format(releases, arch_name))
+        run("tar -xzf /tmp/{1}.tgz -C {0}/{1}/".format(
             releases, arch_name
-        )).failed:
-            raise Exception
+        ))
 
         # deleting the the archive
-        if run("rm /tmp/{}.tgz".format(arch_name)).failed:
-            raise Exception
+        run("rm /tmp/{}.tgz".format(arch_name))
 
         # moving the content of web_static in the new folder
-        if run("mv {0}/{1}/web_static/* {0}/{1}/".format(releases, arch_name)).failed:
-            raise Exception
-        if run("rm -rf {0}/{1}/web_static".format(releases, arch_name)).failed:
-            raise Exception
+        run("mv {0}/{1}/web_static/* {0}/{1}/".format(releases, arch_name))
+        run("rm -rf {0}/{1}/web_static".format(releases, arch_name))
 
         # redirecting the symlink of current to the new folder
-        if run("rm -rf /data/web_static/current").failed:
-            raise Exception
-        if run("ln -s {0}/{1}/ /data/web_static/current".format(
+        run("rm -rf /data/web_static/current")
+        run("ln -s {0}/{1}/ /data/web_static/current".format(
             releases, arch_name
-        )).failed:
-            raise Exception
+        ))
 
         print("New version deployed!")
         return True
