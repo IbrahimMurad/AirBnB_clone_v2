8302f8d147a921595f7ae4eb7ddf19b52fea306a
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-03-22 16:55:09 +0200

    hello HBNB

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This is my first web app using flask
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """ Returns an html page that displays hello HBNB! """
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
