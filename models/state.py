733bd45266e138a2bff6db61c504f8c30c9a205a
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-03-23 23:28:09 +0200

    8. List of states

--- models/state.py
+++ models/state.py
@@ -3,6 +3,7 @@
 from sqlalchemy import Column, String, orm
 from models import storage
 from models.base_model import BaseModel, Base
+from os import getenv
 
 
 class State(BaseModel, Base):
@@ -12,15 +13,15 @@ class State(BaseModel, Base):
 
     cities = orm.relationship("City", cascade="all, delete", backref="state")
 
-    @property
-    def cities(self):
-        """
-        getter attribute that returns a list of cities
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """ getter attribute that returns a list of cities
 in the current state
 """
-        from models.city import City
-        cities_in_state = []
-        for city_obj in storage.all(City).values():
-            if city_obj.state_id == self.id:
-                cities_in_state.append(city_obj)
-        return cities_in_state
+            from models.city import City
+            cities_in_state = []
+            for city_obj in storage.all(City).values():
+                if city_obj.state_id == self.id:
+                    cities_in_state.append(city_obj)
+            return cities_in_state
